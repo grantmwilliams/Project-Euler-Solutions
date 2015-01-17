078edbb0d10b4b15c0b18bc5319dd035268c9eb5
grantmwilliams <grant.williams@okstate.edu>
2015-01-17 13:17:11 -0600

    Problem 22 (My proudest solution)
    This is the first non math oriented problem so I’m very proud that i
    was able to find a solution that worked so efficiently.
    
    I plan on updating the file with a more condensed solution (possibly a
    one liner?) but for now ill just throw this one up!

---
+++ Matlab/NamesScores/NamesScores.m
@@ -0,0 +1,16 @@
+%% Names Scores 22
+clear;close all;clc;
+tic
+load('namesData.mat'); % Load in my names file (replace with your name for it)
+
+% Removes all "" marks from each name and alphabetizes list
+names = sort(strrep( names(1,1:end),'"','')); 
+
+
+final = 0;
+for i = 1:length(names)
+   n = int32(char(names(1,i)))-64;
+ 
+   final = final + sum(n) * i;
+end
+toc
