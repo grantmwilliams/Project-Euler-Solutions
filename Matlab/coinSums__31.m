46f1a93526360960bf6f506d45f5168c58793d1f
grantmwilliams <grant.williams@okstate.edu>
2015-02-16 16:50:11 -0600

    Added 31 - Uses Dynamic Programming
    Very quick and actually working now.

--- Matlab/coinSums__31.m
+++ Matlab/coinSums__31.m
@@ -2,13 +2,21 @@
 clear;close all;clc;
 tic
 
-coins = [1 2 5 10 20 50 100 200];
+coins = [1 2 5 10 20 50 100 200];       % Value of coins we can use
+amount = 201;                           % Matlab indexes from 1 not 0, so we need to add 1 to our target value
+ways = zeros(1,amount);                 % Preallocating for speed
+ways(1) = 1;                            % First solution is 1
 
-
-for i = 1:size(coins,2)
-    combinationsSet{i} = nchoosek(coins,i);
+% Solves from smallest sub problem to largest (bottom up approach of
+% dynamic programming).
+for i = 1:length(coins)                 
+    for j = coins(i)+1:amount           
+        ways(j) = ways(j) + ways(j-coins(i));   
+    end
 end
 
-disp(combinationsSet)
+display(ways(amount));
+
 toc
+
 
