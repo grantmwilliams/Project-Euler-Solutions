2c805c5b9d19742ee60037d264f0e58d960fb4a5
grantmwilliams <grant.williams@okstate.edu>
2015-02-23 15:10:30 -0600

    Added Another Solution to First Project Euler Problem

--- Matlab/problems3and5__1.m
+++ Matlab/problems3and5__1.m
@@ -4,6 +4,7 @@ clear
 %% Slow For Loop
 clear;
 tic
+A = zeros(1,999);
 for i = 1:999
     
     if mod(i,3) == 0 || mod(i,5) == 0
@@ -13,7 +14,7 @@ for i = 1:999
     
 end
 
-answer = sum(A)
+disp(sum(A));
 toc
 
 
@@ -23,13 +24,13 @@ clear
 
 tic
 i = 1:999; 
-answer = sum(i(mod(i,3)==0 | mod(i,5)==0))
+disp(sum(i(mod(i,3)==0 | mod(i,5)==0)));
 toc
 %% Alternative Vectorized Code
 clear
 
 tic
-answer = sum(0:3:999) + sum(0:5:999) - sum(0:15:999)
+disp(sum(0:3:999) + sum(0:5:999) - sum(0:15:999));
 toc
 %% Fastest Version
 clear
@@ -39,5 +40,17 @@ i=999;
 i3=floor(i/3);
 i5=floor(i/5);
 i15=floor(i/15);
-answer = (3*i3*(i3+1) + 5*i5*(i5+1) - 15*i15*(i15+1))/2
+disp((3*i3*(i3+1) + 5*i5*(i5+1) - 15*i15*(i15+1))/2);
+toc
+
+%% New Vectorized Code
+clear
+
+tic
+
+A = 3:3:999;
+B = 5:5:995;
+C = 15:15:990;
+disp(sum(A)+sum(B)-sum(C));
+
 toc
