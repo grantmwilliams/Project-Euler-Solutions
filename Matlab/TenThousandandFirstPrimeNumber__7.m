38e516c40ac30183a0b833f6049027650ea6e08f
grantmwilliams <grant.williams@okstate.edu>
2015-03-04 16:15:15 -0600

    Updated Euler 7 & 8 in Matlab

--- Matlab/TenThousandandFirstPrimeNumber__7.m
+++ Matlab/TenThousandandFirstPrimeNumber__7.m
@@ -1,17 +1,11 @@
 %% 10001 Prime Number
 clear; close all; clc;
 tic
-x = 150000;
-primes = 2:x;
-i=2;
-
-while (i <= x)
-    for i = 2*i:i:x
-        primes(i - 1) = 0;
-    end;
-    i = i + 1;
+i = 10001;
+P = primes(i);
+while length(P) < 10001 || length(P) == 10001
+    i = i + 100;
+    P = primes(i);
 end
-
-primes = primes(primes > 0);
-final = primes(10001)
+disp(P(10001));
 toc
