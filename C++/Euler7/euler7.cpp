f9803a496ff6ce760d1528c95a915d61e03210ee
grantmwilliams <grant.williams@okstate.edu>
2015-03-11 23:02:17 -0500

    Added fast(ish) solution to Euler 7 in C++

--- C++/Euler7/euler7.cpp
+++ C++/Euler7/euler7.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <math.h>
 int main(){
     //Clock Stuff
     std::clock_t start;
@@ -7,11 +7,23 @@ int main(){
     start = std::clock();
     //Stop Clock Stuff
     
+    int i=3,x=2;
+    bool ifPrime=0;
     
+    while (x<10001)
+    {
+        i+=2;
+        for (int div=2;div<=sqrt(i);div++)
+        {
+            if (i%div==0) {ifPrime=0; break;}
+            else ifPrime=1;
+        }
+        if (ifPrime==1) x++;
+    }
     
+    std::cout<<i<<std::endl;
     
-    
-    std::cout << "£2 can be generated in " << ways[200] << " number of ways" << std::endl;
+    //std::cout << "The " << limit << " prime is:" << num << std::endl;
     
     duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
     std::cout<<"Elapsed Time: "<< duration << " seconds" << std::endl;
