4cb5648e7c3fc923c9a9f60fd09aff4f2c03cab2
grantmwilliams <grant.williams@okstate.edu>
2015-03-18 22:49:27 -0500

    Added Euler10 in C++

---
+++ C++/Euler10/euler10.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <ctime> 
+
+using namespace std;
+
+void sieve_atkins(long long int n){
+    long long int sum=0;
+    vector<bool> is_prime(n+1);
+    is_prime[2]=true;
+    is_prime[3]=true;
+    for (long long int i = 5; i <=n; i++) {
+        is_prime[i]=false;
+    }
+    long long int lim = ceil(sqrt(n));
+    for (long long int x = 1; x <= lim; x++) {
+        for (long long int y = 1; y<=lim; y++) {
+            long long int num = (4 * x * x + y * y);
+            if (num <= n && (num % 12 == 1 || num % 12 == 5)) {
+                is_prime[num]=true;
+            }
+            num = (3 * x * x + y * y);
+            if (num <=n && (num%12 == 7)) {
+                is_prime[num]=true;
+            }
+            if (x > y) {
+                num = (3 * x * x - y * y);
+                if (num <= n && (num % 12 == 11)) {
+                    is_prime[num]=true;
+                }
+            }
+        }
+    }
+    for (long long int i = 5; i<=lim; i++) {
+        if (is_prime[i]) {
+            for (long long int j = i*i; j<=n; j+=i) {
+                is_prime[j]=false;
+            }
+        }
+    }
+    
+    for (long long int i = 2; i <= n; i++) {
+        if (is_prime[i]) {
+            sum += i;
+        }
+    }
+    cout << sum<<endl;
+}
+
+int main(){
+    
+    //clock stuff
+    std::clock_t start;
+    double duration;
+    start = std::clock();
+    //stop clock stuff
+    
+    
+    
+    long long int n = 2000000;
+    cout<<"Sum of primes below "<<n<<" is: ";
+    sieve_atkins(n);
+    
+    
+    //clock stuff again
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    
+    std::cout<<"Elapsed time: "<< duration <<" seconds" << endl;
+    return 0;
+    
+}
