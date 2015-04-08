347cfc43a9f77563af5f6ceb893b5ba80b208c8f
grantmwilliams <grant.williams@okstate.edu>
2015-04-08 12:22:56 -0500

    update to Euler 1 in C++

--- C++/Euler1/euler1.cpp
+++ C++/Euler1/euler1.cpp
@@ -6,23 +6,29 @@ int numbers();
 
 int main()
 {
-    int ans = numbers();
-    cout << "\n The sum of the numbers divisible by 3 or 5 and less than 1000 is: ";
-    cout << ans << endl;
-
+    //Clock stuff
+    std::clock_t start;
+    double duration;
+    
+    start = std::clock();
+    
+    //Actual Algorithm Here
+    int sum = 0;
+    for (int i =1; i<1000; i++) {
+        if (i%3 == 0 || i%5 == 0) {
+            sum +=i;
+        }
+    }
+    
+    //Finish Clock Stuff
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    
+    cout<< "Elapsed: "<< duration << " seconds"<< endl;
+    
+    //Print Answer to Algorithm
+    cout << "The Sum of Multiples of 3 and 5 below 1000 is: " << sum << endl;
+    
     return 0;
 }
 
-// Begin Function
-int numbers()
-{
-    int n = 0;
-    for(int i=1;i<1000;i++)
-    {
-        if((i%3)==0||(i%5)==0)
-        {
-            n += i;
-        }
-    }
-    return n;
-}
+
