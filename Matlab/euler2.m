c6cb4d3197a883f98826ef98f49a5fc632904cb6
grantmwilliams <grant.williams@okstate.edu>
2015-04-29 19:46:10 -0500

    Added a small update to euler 2 in matlab

--- Matlab/euler2.m
+++ Matlab/euler2.m
@@ -16,9 +16,30 @@ while(fibonacci(i)<4e6)
     fibonacci(i)=fibonacci(i-1)+fibonacci(i-2);
     
     if mod(fibonacci(i),2)==0
-        answer = answer+fibonacci(i);
+       answer = answer+fibonacci(i);
     end
 end
+
 fprintf('The answer is: %.0d\n',answer);
 toc
 
+
+%% Alternative Method
+clear;close all;clc;
+tic
+
+fibonacci = zeros(1,50);
+fibonacci(1) = 1;
+fibonacci(2) = 2;
+
+i = 2;
+answer = 2;
+
+while(fibonacci(i)<4e6)
+    i=i+1;
+    
+    fibonacci(i)=fibonacci(i-1)+fibonacci(i-2);
+
+end
+disp(sum(fibonacci(mod(fibonacci,2)==0)));
+toc
