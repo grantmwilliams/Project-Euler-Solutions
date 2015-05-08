99bb1d0c9aa9488b50988a165d03fc0912d5d009
grantmwilliams <grant.williams@okstate.edu>
2015-05-08 15:45:33 -0500

    Euler 3 in Mathematica

---
+++ Mathematica/euler3.m
@@ -0,0 +1,12 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*Max@Select[Divisors[600851475143],PrimeQ]//AbsoluteTiming*)
+
+
+(* ::Input:: *)
+(*Max@@Select[FactorInteger[600851475143][[All,1]]]//AbsoluteTiming*)
+
+
+(* ::Input:: *)
+(*Max@FactorInteger[600851475143]//AbsoluteTiming*)
