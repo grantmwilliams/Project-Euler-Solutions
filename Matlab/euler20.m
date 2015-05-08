5ba93da36e657c1f0dfb88c5325554981fc301c6
grantmwilliams <grant.williams@okstate.edu>
2015-05-08 05:11:28 -0500

    Added faster Euler 20 in Matlab

--- Matlab/euler20.m
+++ Matlab/euler20.m
@@ -1,5 +1,12 @@
 %% Factorial Digit Sum 20
 clear; close all; clc;
 tic 
-disp(feval(symengine, 'numlib::sumOfDigits', factorial(sym(100)))); 
+disp(feval(symengine, 'numlib::sumOfDigits', prod(sym(1:100)))); 
 toc
+
+%% Faster Solution (Not Mine)
+clear; close all; clc;
+tic 
+disp(sum(str2num(strread(char(prod(sym(1:100))),'%c')))); 
+toc
+
