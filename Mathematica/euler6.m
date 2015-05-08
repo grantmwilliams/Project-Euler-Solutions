063c6d4079144c1c76d1a47c50554fde06dbe526
grantmwilliams <grant.williams@okstate.edu>
2015-05-08 15:46:02 -0500

    Euler 7 in Mathematica

---
+++ Mathematica/euler6.m
@@ -0,0 +1,8 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*Total[Table[i,{i,100}]]^2-Total[Table[i^2,{i,100}]]//AbsoluteTiming*)
+
+
+(* ::Input:: *)
+(*Sum[k,{k,100}]^2-Sum[k^2,{k,100}]//AbsoluteTiming*)
